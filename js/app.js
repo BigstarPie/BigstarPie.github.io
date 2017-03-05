60c107dcbc98786cd127093b37a97a744225dd4f
BigstarPie <ken91162002@gmail.com>
2017-03-05 18:35:57 +0800

    finish scroll to main

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 $(function(){
     $('#toCH256').on('click', function(){
-        alert('wegwee');
-        var test = $.get('../product/ch256.html');
-        console.log(test);
+        $('#main').load('../product/ch256.html');
+        $('html, body').animate({scrollTop: $('#main').offset().top},'slow');
+        // var test = $.get('../product/ch256.html');
+        // console.log(test);
+        // $('#main').html(test.responseText);
     });
 
 });
