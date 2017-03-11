4757497a866eee33d5ee6d73cc58619bbc1512cb
RogerChang <rock10186@gmail.com>
2017-03-11 21:43:01 +0800

    modelizationService

--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@ $(function(){
     $('#toCH256').on('click', function(){
         $('#main').load('../product/ch256.html');
         $('html, body').animate({scrollTop: $('#main').offset().top},'slow');
-        // var test = $.get('../product/ch256.html');
-        // console.log(test);
-        // $('#main').html(test.responseText);
+    });
+
+    $('#modelizationService').on('click', function(){
+        $('#main').load('../product/modelizationService.html');
+        $('html, body').animate({scrollTop: $('#main').offset().top},'slow');
     });
 
 });
