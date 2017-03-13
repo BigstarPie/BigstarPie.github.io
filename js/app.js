2e09db2e511eccbac1e27b0b94a245542010c7f4
BigstarPie <ken91162002@gmail.com>
2017-03-14 00:14:50 +0800

    update modelizationService link

--- js/app.js
+++ js/app.js
@@ -4,9 +4,23 @@ $(function(){
         $('html, body').animate({scrollTop: $('#main').offset().top},'slow');
     });
 
-    $('#modelizationService').on('click', function(){
-        $('#main').load('../product/modelizationService.html');
-        $('html, body').animate({scrollTop: $('#main').offset().top},'slow');
-    });
+    // $('#modelizationService').on('click', function(){
+    //     $('#main').load('../product/modelizationService.html');
+    //     $('html, body').animate({scrollTop: $('#main').offset().top},'slow');
+    // });
 
+    // $('a[href="#modelizationService"]').on('click', function () {
+    //     $('#main').hide();
+    //     $('#modelizationService').show();
+    // });
+
+    // $('a').on('click', function(){
+    //     if($(this).attr('href')!== '#modelizationService'){
+    //         $('#main').show();
+    //         $('#modelizationService').hide();
+    //     } else{
+    //         $('#main').hide();
+    //         $('#modelizationService').show();
+    //     }
+    // });
 });
